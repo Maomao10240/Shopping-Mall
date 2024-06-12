95b9b9099c39edbb8f506de8f04cdda0e23f9c71
Maomao10240 <mhpan.tju@gmail.com>
2024-06-12 10:54:35 -0700

    initial add

---
+++ order/src/test/java/com/maohua/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maohua.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
